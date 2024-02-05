a35534f14025af223c62fddfa22a633f0a583b49
Ivan Villacorta <ivan_mv23@hotmail.com>
2024-02-05 16:08:31 -0300

    defer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IfComponent } from './components/control-flow/if/if.component';
 import { ForComponent } from './components/control-flow/for/for.component';
+import { DeferComponent } from './components/defer/defer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IfComponent, ForComponent],
+  imports: [RouterOutlet, IfComponent, ForComponent, DeferComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
